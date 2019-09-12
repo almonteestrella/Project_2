4bb3bea6ca0f8ce2537764d961881cf0a143bcf1
philblanchard <phil@philblanchard.com>
2019-09-12 19:13:49 -0400

    added db model

--- models/bodysafe.js
+++ models/bodysafe.js
@@ -1,14 +1,43 @@
 module.exports = function(sequelize, DataTypes){
     var Review = sequelize.define("review", {
-        inspectid: DataTypes.INTEGER,
-        status: DataTypes.STRING,
+        _id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        estStatusDesc: DataTypes.STRING,
+        enfrID: DataTypes.INTEGER,
+        inspID: DataTypes.INTEGER,
+        infrTypeCD: DataTypes.INTEGER,
         postal: DataTypes.STRING,
+        rowNo: DataTypes.INTEGER,
         addr: DataTypes.STRING,
-        name: DataTypes.STRING,
+        estName: DataTypes.STRING,
         infrAction: DataTypes.STRING,
+        lon: {
+            type: DataTypes.DECIMAL(11,8),
+            allowNull: false
+        },
+        servID: DataTypes.INTEGER,
+        enfrOurcame: DataTypes.STRING,
+        unit: DataTypes.STRING,
+        infrTypeDesc: DataTypes.STRING,
+        enfrAmountFined: DataTypes.INTEGER,
+        inspStatusDesc: DataTypes.STRING,
+        enfrOutcomeDate: DataTypes.DATE,
+        lat: {
+            type: DataTypes.DECIMAL(10,8),
+            allowNull: false
+        },
+        servTypeCD: DataTypes.INTEGER,
+        inspDate: DataTypes.DATE,
+        infrID: DataTypes.INTEGER,
+        addrID: DataTypes.INTEGER,
         estID: DataTypes.INTEGER,
-        inspectionDate: DataTypes.DATE,
-        ifrID: DataTypes.INTEGER
+        servTypeDesc: DataTypes.STRING,
     })
     return Review;
 }
+
+
+
+
