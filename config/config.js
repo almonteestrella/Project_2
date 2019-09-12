ba4d31a8eb280b5af3ecb46b3a7fb27351b6e249
Héctor Morales <hector@morales.is>
2019-09-12 12:27:36 -0400

    use .env credentials

---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: process.env.user,
+    password: process.env.pass,
+    database: process.env.db,
+    host: process.env.host,
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  test: {
+    username: process.env.user,
+    password: process.env.pass,
+    database: process.env.db,
+    host: process.env.host,
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  production: {
+    username: process.env.user,
+    password: process.env.pass,
+    database: process.env.db,
+    host: process.env.host,
+    dialect: "mysql",
+    operatorsAliases: false
+  }
+};
