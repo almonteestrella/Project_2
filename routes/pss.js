83ac73f082f9bb29ca083bb2424b1ba1af0f6331
Héctor Morales <hector@morales.is>
2019-09-12 23:37:11 -0400

    some filtering

--- routes/pss.js
+++ routes/pss.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 var db = require("../models/index");
 
 /* GET users listing. */
-router.get("/lat/:lat/lon/:lon/:cat?", function(req, res, next) {
+router.get("/lat/:lat/lon/:lon/:cat?", function (req, res, next) {
   let lat = parseFloat(req.params.lat);
   let lon = parseFloat(req.params.lon);
   let cat = req.params.cat || "all";
@@ -13,9 +13,9 @@ router.get("/lat/:lat/lon/:lon/:cat?", function(req, res, next) {
 });
 
 // return list of items
-router.get("/r/:itmes", function(req, res, next) {
+router.get("/r/:itmes", function (req, res, next) {
   let dataset = require("../dataset/BodySafe.json");
-  let data = []
+  let data = [];
   for (i = 0; i < req.params.itmes; i++) {
     data.push(dataset[i]);
   }
@@ -25,7 +25,25 @@ router.get("/r/:itmes", function(req, res, next) {
   res.send(JSON.stringify(data));
 });
 
-router.get("/all", function(req, res, next) {
+// return list of items
+router.get("/f", function (req, res, next) {
+  let dataset = require("../dataset/BodySafe.json");
+  let data = {};
+  for (i = 0; i < dataset.length; i++) {
+    // console.log('id'+dataset[i].servID)
+    data["id"+dataset[i].servID] = {
+      name: dataset[i].estName,
+      lat: dataset[i].lat,
+      lon: dataset[i].lon
+    };
+  }
+
+  // console.log(JSON.stringify(data))
+
+  res.send(JSON.stringify(data));
+});
+
+router.get("/all", function (req, res, next) {
   let dataset = require("../dataset/BodySafe.json");
 
   // console.log(JSON.stringify(data))
