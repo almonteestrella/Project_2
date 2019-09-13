3208e8f9b8f80f46085be8b13573f876830cd2ed
Héctor Morales <hector@morales.is>
2019-09-12 22:55:46 -0400

    dataset test

--- routes/pss.js
+++ routes/pss.js
@@ -1,14 +1,36 @@
 var express = require("express");
 var router = express.Router();
+var fs = require("fs");
 
 var db = require("../models/index");
 
 /* GET users listing. */
-router.get("/:lat/:lon/:cat?", function(req, res, next) {
+router.get("/lat/:lat/lon/:lon/:cat?", function(req, res, next) {
   let lat = parseFloat(req.params.lat);
   let lon = parseFloat(req.params.lon);
   let cat = req.params.cat || "all";
   res.send("lat: " + lat + ", long: " + lon + ", category: " + cat);
+});
+
+// return list of items
+router.get("/r/:itmes", function(req, res, next) {
+  let dataset = require("../dataset/BodySafe.json");
+  let data = []
+  for (i = 0; i < req.params.itmes; i++) {
+    data.push(dataset[i]);
+  }
+
+  // console.log(JSON.stringify(data))
+
+  res.send(JSON.stringify(data));
+});
+
+router.get("/all", function(req, res, next) {
+  let dataset = require("../dataset/BodySafe.json");
+
+  // console.log(JSON.stringify(data))
+
+  res.send(JSON.stringify(dataset));
 });
 
 // category
