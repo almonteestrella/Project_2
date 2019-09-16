8349e687a4f532786b895cc8ff3bfc8a0adad2eb
Héctor Morales <hector@morales.is>
2019-09-16 17:25:20 -0400

    update grouping for category results

--- routes/pss.js
+++ routes/pss.js
@@ -61,7 +61,7 @@ router.get("/db/:catego", function(req, res, next) {
       where: {
         serv_type_desc: [catego]
       },
-      group: ["est_name"]
+      group: ["lat", "lon", "est_name", "serv_type_desc"]
     })
     .then(results => res.send(JSON.stringify(results)));
 });
