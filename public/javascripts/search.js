42eac4a48b77499f0106a94533d9250c8f6f8496
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-09-17 19:10:06 -0400

    Update popup content by type search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -5,6 +5,7 @@ $(function() {
     let val = $("#search-box")
       .val()
       .trim();
+      
     if (val !== "") {
       $.get("/api/s2/" + val, function() {}).done(function(data) {
         let items = jQuery.parseJSON(data);
@@ -13,16 +14,45 @@ $(function() {
           let latlng = L.latLng(items[key].lat, items[key].lon);
           let marker = L.marker(latlng, { icon: iconresult }).addTo(mymap);
 
-          //always open popup window below the search options
           marker.addEventListener("click", function(e) {
-            L.popup({
-              autoPanPaddingTopLeft: [0, 200],
-              offset: [0, -23]
+
+            console.log(this);
+            let markerClicked = this;
+            let infraction = "";
+            let status = "";
+            let content = "";  
+
+            //get inspection history
+            $.get("/api/review/" + items[key].name, function () {
             })
-              .setLatLng(this.getLatLng())
-              .setContent(items[key].cat + "<br>" + items[key].name)
-              .openOn(mymap);
+              .done(function (data)  {
+                let items_2 = jQuery.parseJSON(data);
+                status = items_2[0].status;
+                infraction = items_2[0].infraction;
+                if( infraction === null) {
+                  infraction = "No infraction!";
+                }
+                content = `<div class="cat">${items[key].cat}</div>
+                <div class="name">${items[key].name}</div>
+                <div class="status">${status}</div>
+                <div>${infraction}</div>`;
+              })
+                .then(function() {
+
+                  //define popup window 
+                  popup = L.popup({
+                    autoPanPaddingTopLeft: [0, 200],    //open popup window below the search options
+                    offset: [0, -23],   //adjust the distance from the marker
+                    maxWidth: 270
+                  })
+                    .setLatLng(markerClicked.getLatLng())
+                    .setContent(content)
+                    .openOn(mymap);
+                });
+
           });
+
+          
         }
       });
     }
